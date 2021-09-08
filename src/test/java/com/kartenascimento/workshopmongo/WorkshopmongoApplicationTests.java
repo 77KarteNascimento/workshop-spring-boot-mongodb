6ab63e7e42025eff51cd5c772d7bc625251edff1
Karte Nascimento <ti.kartenascimento@gmail.com>
2021-09-08 17:32:11 -0400

    primeiro commit - projeto criado

---
+++ src/test/java/com/kartenascimento/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kartenascimento.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
